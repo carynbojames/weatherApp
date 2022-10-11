19e9507d11d3e0a46b9e7612789699aa7a9f2274
carynbojames <110750060+carynbojames@users.noreply.github.com>
2022-10-11 02:22:03 -0500

    Create separate functions for each button to assign a value to city based on its event listener. Create one common function for adding another button to the list and pull weather API data.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,18 @@
 // --- Create History Button --- 
 // var citySearchUser = $('#search'); 
 var cityHistory = $('#city-list'); // parent
+var city
 // cityList.append('<li class="city-titles">Chicago</li>'); // add child to cityList parent
 
-
-
-// create element
-function cityList(event) {
-    event.preventDefault();
-    var city = $('input[name="city"]').val(); // get value from form
+function weather() {
+    console.log('Weather City: ' + city)
     citySearch = $('<button>'); // Previously missing the <>
     citySearch.addClass('city-titles');
     citySearch.attr('name',city)
     citySearch.text(city);
     cityHistory.append(citySearch);
-    /// ACTION: Make the new value a button
-    /// ACTION: Make the previously entered value disappear
-    /// ACTION: Create a message if search is entered w/o a value
     // cityList.text(city); // When I add this, it replaces the previous value
     // cityList.append('<li class="city-titles>' + city + '</li>'); // add child to cityList parent
-    // weather();
 
     // --- Get the latitude and longitude ---
     var apiKey = 'eea82704764516c62016fa4ce2668513';
@@ -31,13 +24,15 @@ function cityList(event) {
         })
         .then(function(data){
             console.log(data);
-            console.log(data[0].lat)
-            console.log(data[0].lon)
+            // console.log(data[0].lat)
+            // console.log(data[0].lon)
 
             // --- Get the future forecast
             var lat = data[0].lat
             var lon = data[0].lon
             var weatherFuture = 'http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + lat + '&lon=' + lon + '&cnt=5&appid=' + apiKey;
+            console.log('Lat:' + lat)
+            console.log('Lon: ' + lon)
             console.log(weatherFuture)
 
             // fetch(weatherFuture)
@@ -47,46 +42,35 @@ function cityList(event) {
             //     .then(function(data){
             //         console.log(data);
             //     })
-        })
-    
-    $('input[name="city"').val('');
+        })    
 }
 
-function cityListEl(event) {
+// Create city history
+function cityForm(event) {
     event.preventDefault();
-    var city = $(event.target).attr('name');
+    city = $('input[name="city"]').val(); // get value from form
+    $('input[name="city"').val('');
+    weather();
+}
+
+function cityList(event) {
+    event.preventDefault();
+    city = $(event.target).attr('name');
     // console.log('event target: ' + event.tar]get)
     // console.log('test: ' + test)
     // var city = $('.name');
     // console.log(event)
     // console.log('This: ' + this)
-    console.log('City List: ' + city)
-    citySearch = $('<button>'); // Previously missing the <>
-    citySearch.addClass('city-titles');
-    citySearch.attr('name',city)
-    citySearch.text(city);
-    cityHistory.append(citySearch);
+    weather();
+}
 
-    var apiKey = 'eea82704764516c62016fa4ce2668513';
-    var queryLatLon = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + apiKey;
-
-    fetch(queryLatLon)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function(data){
-            console.log(data);
-            console.log(data[0].lat)
-            console.log(data[0].lon)
-        })
-    }       
 
 var search = $('#searchBtn'); 
-search.on('click', cityList);
+search.on('click', cityForm);
 /// QUESTION: Can you make the click execute two functions? 
 
 var searchHistory = $('#search-history');
-searchHistory.on('click', '.city-titles', cityListEl); // no response[]
+searchHistory.on('click', '.city-titles', cityList); // no response[]
 
 // var currentTemp = $('#current-temp')
 // currentTemp.text('test')
