e956fae79c8aeb106cccce2275cbbfc76c2f7b04
carynbojames <110750060+carynbojames@users.noreply.github.com>
2022-10-11 02:40:09 -0500

    Merge API code from test files to project file

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var city
 // cityList.append('<li class="city-titles">Chicago</li>'); // add child to cityList parent
 
 function weather() {
+    // --- Create a new button
     console.log('Weather City: ' + city)
     citySearch = $('<button>'); // Previously missing the <>
     citySearch.addClass('city-titles');
@@ -14,25 +15,25 @@ function weather() {
     // cityList.text(city); // When I add this, it replaces the previous value
     // cityList.append('<li class="city-titles>' + city + '</li>'); // add child to cityList parent
 
-    // --- Get the latitude and longitude ---
     var apiKey = 'eea82704764516c62016fa4ce2668513';
+
+    // --- Get the latitude and longitude
     var queryLatLon = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + apiKey;
 
     fetch(queryLatLon)
         .then(function (response) {
             return response.json();
         })
+
         .then(function(data){
             console.log(data);
             // console.log(data[0].lat)
             // console.log(data[0].lon)
 
-            // --- Get the future forecast
             var lat = data[0].lat
             var lon = data[0].lon
+    
             var weatherFuture = 'http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + lat + '&lon=' + lon + '&cnt=5&appid=' + apiKey;
-            console.log('Lat:' + lat)
-            console.log('Lon: ' + lon)
             console.log(weatherFuture)
 
             // fetch(weatherFuture)
@@ -42,7 +43,33 @@ function weather() {
             //     .then(function(data){
             //         console.log(data);
             //     })
-        })    
+
+            console.log('Lat:' + lat)
+            console.log('Lon: ' + lon)
+
+            // --- Get the future forecast
+            var queryFuture = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon='+ lon + '&appid=' + apiKey + '&units=imperial';
+
+            fetch(queryFuture)
+                .then(function (response) {
+                    return response.json(); 
+                })
+
+                .then(function(data) {
+                    for (var i = 1; i < 6; i++) {
+                        var forecast = $('#forecast')
+                        var forecastCard = $('<card>')
+                        var date = moment().add(i, 'days').format('M/D/YY');
+                        forecastCard.text('Forecast Day: ' + date) 
+                        forecast.append(forecastCard)
+                        forecastCard.append('<p>Temp: ' + data.list[i].main.temp + ' F</p>')
+                        forecastCard.append('<p>Wind: '  + data.list[i].wind.speed + ' mph</p>')
+                        forecastCard.append('<p>Humidity: ' + data.list[i].main.humidity + ' %</p>')
+                        forecastCard.append('<p>Icon: ' + data.list[i].weather[0].description + '</p>')
+                    }
+                    console.log(data)
+                })
+        })   
 }
 
 // Create city history
